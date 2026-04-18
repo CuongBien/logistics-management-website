44494c675dcdeb8beabd2a5728e312c61a2ab54a
Cuong Bien <biencaocuongg@gmail.com>
2026-04-18 21:31:33 +0700

    Outbound + Inbound implemented + Swagger fix a bit + cleaning code

--- src/Services/Ordering/Ordering.Infrastructure/Persistence/Migrations/20260418140910_CheckPendingOrdering.Designer.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/Migrations/20260418140910_CheckPendingOrdering.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Ordering.Infrastructure.Persistence;
 
 #nullable disable
 
-namespace Ordering.Infrastructure.Migrations
+namespace Ordering.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260418140910_CheckPendingOrdering")]
+    partial class CheckPendingOrdering
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Services/Warehouse/Warehouse.Infrastructure/Migrations/20260418140834_UpdateBinWithOrderId.Designer.cs
+++ src/Services/Warehouse/Warehouse.Infrastructure/Migrations/20260418140834_UpdateBinWithOrderId.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Warehouse.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Warehouse.Infrastructure.Persistence;
 namespace Warehouse.Infrastructure.Migrations
 {
     [DbContext(typeof(WMSDbContext))]
-    partial class WMSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260418140834_UpdateBinWithOrderId")]
+    partial class UpdateBinWithOrderId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
