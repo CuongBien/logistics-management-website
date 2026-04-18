1b92d48bf502d0dbb47b7d4fe3c5b6e6c63c4473
Luc050606 <tanluctcv06@gmail.com>
2026-04-18 10:31:28 +0700

    Add files via upload

---
+++ src/Services/Warehouse/Warehouse.Application/Features/Inbound/Commands/ReceiveInboundItem/ReceiveInboundItemCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Warehouse.Application.Features.Inbound.Commands.ReceiveInboundItem;
+
+public class ReceiveInboundItemCommandValidator : AbstractValidator<ReceiveInboundItemCommand>
+{
+    public ReceiveInboundItemCommandValidator()
+    {
+        RuleFor(x => x.ReceiptId)
+            .NotEmpty().WithMessage("ReceiptId must not be empty.");
+
+        RuleFor(x => x.OrderId)
+            .NotEmpty().WithMessage("OrderId must not be empty.");
+
+        RuleFor(x => x.BinCode)
+            .NotEmpty().WithMessage("BinCode must not be empty.");
+
+        RuleFor(x => x.ScannedBy)
+            .NotEmpty().WithMessage("ScannedBy must not be empty.");
+    }
+}
